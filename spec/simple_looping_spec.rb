687f9d4c5a4821256e9a4ab3ad0756b56ab3d929
zhenggao cai <caizhengao1992@gmail.com>
2019-04-14 04:05:37 +0000

    Automatically backed up by Learn

--- spec/simple_looping_spec.rb
+++ spec/simple_looping_spec.rb
@@ -1,5 +1,5 @@
 
-require
+require_relative '.'
 describe 'Loops' do
   before(:each) do
     @looping_string = "Welcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\n"
